84be34080d12bdfac257a21d54104ca4f812bd48
unknown <Student@DIGITALSCHOOL.LOCAL>
2026-02-28 14:47:38 +0100

    .

---
+++ aboutus.php
@@ -0,0 +1,22 @@
+<?php
+/* 
+Template Name: About Us
+Template Post Type: page
+*/
+?>
+
+<?php get_header():?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello this is about us page</h1>
+</body>
+</html>
+
+<?php get_footer();?>
